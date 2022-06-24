6e093f9ff422db7065de86705f9add196cd15364
Lee-moran <leemoran06@hotmail.com>
2022-06-24 13:24:51 +0000

    error fixed done = true

--- todo/test_views.py
+++ todo/test_views.py
@@ -7,14 +7,14 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/todo_list.html')
-
+    
     def test_get_add_item_page(self):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
     def test_get_edit_item_page(self):
-        item = Item.objects.create(name='Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/edit_item.html')
@@ -24,7 +24,7 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
 
     def test_can_delete_item(self):
-        item = Item.objects.create(name='Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
         existing_items = Item.objects.filter(id=item.id)
@@ -36,3 +36,4 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
