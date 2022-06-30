7c275f93ce07ab48092092f9a45133d31186ca43
Lee-moran <leemoran06@hotmail.com>
2022-06-30 12:33:51 +0000

    setup development enviorment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import dj_database_url
 
 development = os.environ.get('DEVELOPMENT', False)
- 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
